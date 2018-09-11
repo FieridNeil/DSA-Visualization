dd99708f82d2ed9f5e9ee36a83a43f08572cb642
Fierid Neil <fieridneil@gmail.com>
2018-09-10 17:42:32 -0700

    commit

---
+++ control.py
@@ -0,0 +1,95 @@
+import pygame as pg
+
+
+class Control():
+    def __init__(self, window, x, y, w, h, bgColor, borderColor, borderThickness):
+        # The window (screen) that will be drawn on
+        self.window = window
+        self.x = x;
+        self.y = y;
+        self.w = w;
+        self.h = h;
+        self.bgColor = bgColor
+        self.borderColor = borderColor
+        self.borderThickness = borderThickness
+        self.rect = pg.Rect(self.x, self.y, self.w, self.h) # Needed for interaction (button click, mouseover, etc), not needed for decoration
+        self.surface = pg.Surface((self.w, self.h))
+        self.surface.fill(self.bgColor)
+
+    def addText(self, text):
+        self.font = pg.font.SysFont('Arial', 25)
+        self.text = self.font.render(text, True, (100,120,0))
+        self.text_rect = self.text.get_rect()
+        self.text_rect.center = (self.w/2, self.h/2)
+        self.surface.blit(self.text, self.text_rect)
+
+    def draw(self):
+        # Draw a rect on top of self.surface at position (0,0) relative to the self.surface.  It is different than the self.rect declared above
+        # self.rect = pg.Rect(self.x, self.y, self.w, self.h) # Recreating when x, y, w, h change
+        # self.surface = pg.Surface((self.w, self.h)) # Recreating when x, y, w, h change
+
+        pg.draw.rect(self.surface, self.borderColor, [0, 0, self.w, self.h], self.borderThickness)
+        self.window.blit(self.surface, (self.x, self.y))
+# End class Control
+
+
+class Button(Control):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    def addText(self, *args, **kwargs):
+        super().addText(*args, **kwargs)
+
+    def click(self, event, callback, *args, **kwargs):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            (mouseX, mouseY) = pg.mouse.get_pos()
+            if(self.rect.collidepoint((mouseX, mouseY))):
+                callback(*args, **kwargs)
+
+# End class button
+
+# Textbox
+class TextBox(Control):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.active = False
+        self.text = ''
+        self.fontsize = 30
+        self.font = pg.font.Font(None, self.fontsize)
+        self.text_surface = self.font.render(self.text, True, (255, 0 , 100))
+
+
+
+    def handle(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(pg.mouse.get_pos()):
+                self.active = True
+                print(self.active)
+            else:
+                self.active = False
+
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                # When return key is pressed
+                if event.key == pg.K_RETURN:
+                    self.text = ''
+                # Delete character
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+
+            # To clear the text or delete character when users press backspace, we fill the surface first with a solid color
+            self.surface.fill(self.bgColor)
+            # Then we update the surface with text
+            self.text_surface = self.font.render(self.text, True, (255, 0, 10))
+
+    def getText(self):
+        return self.text
+
+    def draw(self):
+        self.surface.blit(self.text_surface, (0, self.h / 2 - self.fontsize / 2))
+        super().draw()
+
+# End class TextBox
--- test3.py
+++ test3.py
@@ -1,25 +1,59 @@
-class A():
-    def __init__(self):
-        self.var = "Hi"
+#FIRST \CodeArrayFirst
+class Array:
+    def __init__(self, length, default):
+        if length < 0:
+            raise ValueError
+        self.length = length
+        self.elements = [default] * length
+    def __len__(self):
+        return self.length
+    def __iter__(self):
+        return iter(self.elements)
+    def __getitem__(self, i):
+        if i < 0 or i >= self.length:
+            raise IndexError
+        return self.array[i]
+    def __setitem__(self, i, x):
+        if i < 0 or i >= self.length:
+            raise IndexError
+        self.array[i] = x
+#LAST \CodeArrayLast
 
-    def show(self):
-        print(self.var)
+class Vector:
+    def __init__(self, length, default):
+        self.array = Array(0, default)
+        self.in_use = 0
+        for i in range(length):
+            self.add_back(default)
+    def __len__(self):
+        return self.in_use
+    def __iter__(self):
+        return VectorIter(self)
+    def __getitem__(self, i):
+        if i < 0 or i >= self.in_use:
+            raise IndexError
+        return self.array[i]
+    def __setitem__(self, i, x):
+        if i < 0 or i >= self.in_use:
+            raise IndexError
+        self.array[i] = x
+    def add_back(self, x):
+        if self.in_use == len(self.array):
+            self._resize(len(self.array) * 2)
+        self.array[self.in_use] = x
+        self.in_use += 1
+    def remove_back(self):
+        if self.in_use == 0:
+            raise UnderflowError
+        self.in_use -= 1
+        if (3 * self.in_use) < len(self.array):
+            self._resize(self._capacity() // 2)
+    def _resize(self, new_capacity):
+        new_array = Array(new_capacity, None)
+        for i in range(self.in_use):
+            new_array[i] = self.array[i]
+        self.array = new_array
+#LAST \CodeVectorLast
 
 
-class B(A):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-
-
-    def update(self, *args, **kwargs):
-        for thing in args:
-            self.var += thing
-
-    def show(self):
-        super().show()
-
-
-b = B('!', ' ', 'how are you?')
-b.update('\nI am fine, how about you?')
-b.var += "\nI'm great!"
-b.show()
+v = Vector(10, 0)

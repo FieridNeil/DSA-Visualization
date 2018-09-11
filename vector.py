5e0baadf215e3f083dbb9d1def7b81896cd5d56b
Fierid Neil <fieridneil@gmail.com>
2018-09-10 18:31:41 -0700

    commit

--- vector.py
+++ vector.py
@@ -14,10 +14,11 @@ window.blit(sur1, (0, 100))
 label = Control(window, 0, 0, 100, 30, (255, 255, 255), (255, 255, 255), 1)
 label.addText('Elements: ')
 
-input = TextBox(window, 100, 0, 150, 30, (255, 255, 25), (0, 0, 0), 1)
+input = TextBox(window, 100, 0, 200, 30, (255, 255, 25), (0, 0, 0), 1)
 generateBtn = Button(window, 300, 0, 100, 30, (255, 255, 255), (231, 21, 32), 1)
 generateBtn.addText('Generate')
-
+Btn = Button(window, 400, 0, 100, 30, (255, 255, 255), (231, 21, 32), 1)
+Btn.addText('add back')
 def drawbox(items):
     sur1.fill((0, 255, 255))
     for i,val in enumerate(items):
@@ -25,7 +26,30 @@ def drawbox(items):
         box.addText(str(val))
         box.draw()
 
+class Vector:
+    ''' @s: the surface that will be drawn on
+        @elms: a list of elements
+    '''
+    def __init__(self, s):
+        self.s = s
+        self.elms = []
 
+    def addElms(self, elms):
+        self.elms = elms
+
+    def addBack(self, *args):
+        self.elms.append(args[0])
+        self.draw()
+
+    def draw(self):
+        self.s.fill((0, 255, 255))
+        for i,val in enumerate(self.elms):
+            box = Control(self.s, i * 100, 0, 100, 100, (255, 255, 255), (255, 0, 0), 1)
+            box.addText(str(val))
+            box.draw()
+# End class Vector
+
+v = Vector(sur1)
 
 
 while running:
@@ -33,16 +57,18 @@ while running:
     clock.tick(10)
     for event in pg.event.get():
         input.handle(event)
-        generateBtn.click(event, drawbox, input.getText().split())
+        generateBtn.click(event, v.addElms, input.getText().split())
+        Btn.click(event, v.addBack, 0)
         if event.type == pg.QUIT:
             running = False
     #End event
 
     window.blit(sur1, (0, 100))
-
+    v.draw()
     label.draw()
     input.draw()
     generateBtn.draw()
+    Btn.draw()
     pg.display.flip()
 
 
